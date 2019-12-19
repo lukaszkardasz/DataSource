f15e6f0cd4ad2df99bfd00436dddff9ee4a1b6f2
n2God <kardasz.lukasz@gmail.com>
2019-12-19 13:51:34 +0100

    Servlet added

--- src/pl/n2god/data_source/DbUtil.java
+++ src/pl/n2god/data_source/DbUtil.java
@@ -1,8 +1,45 @@
 package pl.n2god.data_source;
 
+
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 /**
  * @author n2god on 19/12/2019
  * @project DataSource
+ * Rekord do bazy wstawiamy na podstawie wszystkich przekazanych parametrów z formularza, natomiast usuwanie odbywa się
+ * na podstawie nazwy miasta. Zauważ, że przy usuwaniu najpierw odpytujemy bazę, czy dany rekord w ogóle istnieje.
+ * Do konkretnych kolumn w obiekcie ResultSet możemy się odwoływać nie tylko poprzez nazwy kolumn, czyli np. resultSet.getInt("ID"),
+ * ale również po indeksach kolumn, co wykorzystaliśmy w naszym przykładzie -  resultSet.getInt(1).W odróżnieniu od
+ * większości rzeczy w Javie, kolumny w ResultSet indeksowane są od 1 a nie od 0.
+ * Po zakończeniu operacji wstawiania lub usuwania wszystkie zasoby są zwalniane dzięki wykorzystaniu bloku try-with-resources.
  */
 public class DbUtil {
+	public static void insert(String name, String country, String district, int population) throws SQLException {
+		try (Connection connection = ConnectionProvider.getConnection();
+		     Statement statement = connection.createStatement();) {
+
+			String query = String
+					.format("INSERT INTO city(Name, CountryCode, District, Population) VALUES ('%s', '%s', '%s', '%d')",
+							name, country, district, population);
+			statement.executeQuery(query);
+		}
+	}
+	public static void delete (String name) throws SQLException {
+		String selectQuery = String
+				.format("SELECT ID FROM city WHERE Name='%s'", name);
+
+		try (Connection connection = ConnectionProvider.getConnection();
+		     Statement statement = connection.createStatement();
+		     ResultSet resultSet = statement.executeQuery(selectQuery);) {
+			if (resultSet.next()){
+				String deleteQuery = String
+						.format("DELETE FROM city WHERE ID='%d'", resultSet.getInt(1));
+				statement.executeQuery(deleteQuery);
+			}
+		}
+	}
 }
--- src/pl/n2god/servlet/ControllerServlet.java
+++ src/pl/n2god/servlet/ControllerServlet.java
@@ -1,23 +1,62 @@
 package pl.n2god.servlet;
 
+import pl.n2god.data_source.DbUtil;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.sql.SQLException;
 
 /**
  * @author n2god on 19/12/2019
  * @project DataSource
+ * W klasie ControllerServlet obsługujemy żądanie POST odebrane z formularza. Najpierw odczytujemy przesłane parametry,
+ * a następnie w zależności od tego, czy użytkownik wybrał opcję "Dodaj", czy "Usuń" próbujemy dodać lub usunąć rekord
+ * z bazy danych wywołując odpowiednią metodę klasy DbUtil. Po drodze może pojawić się wiele wyjątków - np. użytkownik
+ * mógł nie wypełnić wszystkich pól, a kolumny w bazie danych mają zabezpieczenie przed wstawianiem wartości NULL co
+ * spowoduje błąd. Ze względu na czytelność przykładu pominęliśmy jednak obsługę tych specyficznych sytuacji, ograniczając
+ * się jedynie do weryfikacji wyjątku NumberFormatException - zachęcam potraktować obsługę pozostałych jako ćwiczenie.
+ *
+ * Po przetworzeniu zapytania do bazy danych ustawiamy komunikat, czy rekord został dodany, czy usunięty z bazy i
+ * przekazujemy go do strony message.jsp.
  */
 @WebServlet(name = "ControllerServlet", urlPatterns = "/ControllerServlet")
 public class ControllerServlet extends HttpServlet {
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.setCharacterEncoding("UTF-8");
 
-	}
+		String name = request.getParameter("city");
+		String country = request.getParameter("country");
+		String district = request.getParameter("district");
+		String populationString = request.getParameter("population");
+		int population = 0;
+		if (populationString != null && !populationString.isEmpty()) {
+			population = Integer.parseInt(populationString);
+		}
 
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+		String option = request.getParameter("option");
+		String message = null;
+		if ("add".equals(option)) {
+			try {
+				DbUtil.insert(name, country, district, population);
+				message = "Do bazy dodano miasto " + name;
+			} catch (SQLException e) {
+				message = "Nie udało się dodać miasta " + name;
+				e.printStackTrace();
+			}
+		} else if ("delete".equals(option)){
+			try {
+				DbUtil.delete(name);
+				message = "Pomyślnie usunięto miasto " + name;
+			} catch (SQLException e) {
+				message = "Nie udało się usunąć miasta " + name;
+				e.printStackTrace();
+			}
+		}
+		request.setAttribute("message", message);
+		request.getRequestDispatcher("message.jsp").forward(request, response);
 	}
 }
